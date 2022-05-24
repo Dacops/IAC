e6f4a2ddededf7e5ce568d81249c47ed3c4d5b67
David Pires <79417054+Dacops@users.noreply.github.com>
2022-05-24 14:21:36 +0100

    Update grupo06.asm

--- grupo06.asm
+++ grupo06.asm
@@ -1,86 +1,86 @@
 ; ***********************************************************************
-; * Projeto Intermédio IAC 2021/22										*
-; * Grupo 06															*
-; * Elementos:															*
-; * 	-> David Pires, nº 103458										*
-; *		-> Diogo Miranda, nº 102536										*
-; *		-> Mafalda Fernandes, nº 102702									*
-; *																		*
-; * Modulo:		grupo06.asm												*
-; * Descrição: 	Código assembly relativo ao Projeto Intermédio de IAC 	*
-; *				2021/22, pronto a ser carregado no simulador.			*
+; * Projeto IntermÃ©dio IAC 2021/22					*
+; * Grupo 06								*
+; * Elementos:								*
+; * 	-> David Pires, nÂº 103458					*
+; *	-> Diogo Miranda, nÂº 102536					*
+; *	-> Mafalda Fernandes, nÂº 102702					*
+; *									*
+; * Modulo:	grupo06.asm						*
+; * DescriÃ§Ã£o: 	CÃ³digo assembly relativo ao Projeto IntermÃ©dio de IAC 	*
+; *		2021/22, pronto a ser carregado no simulador.		*
 ; ***********************************************************************
 
 
 ; ***********************************************************************
-; * Endereços de Periféricos											*
+; * EndereÃ§os de PerifÃ©ricos						*
 ; ***********************************************************************
-DISPLAYS   EQU 0A000H  ; endereço dos displays de 7 segmentos (periférico POUT-1)
-TEC_LIN    EQU 0C000H  ; endereço das linhas do teclado (periférico POUT-2)
-TEC_COL    EQU 0E000H  ; endereço das colunas do teclado (periférico PIN)
+DISPLAYS   EQU 0A000H  ; endereÃ§o dos displays de 7 segmentos (perifÃ©rico POUT-1)
+TEC_LIN    EQU 0C000H  ; endereÃ§o das linhas do teclado (perifÃ©rico POUT-2)
+TEC_COL    EQU 0E000H  ; endereÃ§o das colunas do teclado (perifÃ©rico PIN)
 
 
 ; ***********************************************************************
-; * Código																*
+; * CÃ³digo								*
 ; ***********************************************************************
 PLACE      0100H
 
 SP_inicial:				
-	PLACE	0			; o código tem de começar em 0000H
+	PLACE	0			; o cÃ³digo tem de comeÃ§ar em 0000H
 inicio:
-	MOV  SP, SP_inicial		; inicialização de SP
+	MOV  SP, SP_inicial		; inicializaÃ§Ã£o de SP
 
 ; corpo principal do programa
 ciclo:
 	CALL teclado
 	JMP ciclo
 
-; obtém tecla premida
+; obtÃ©m tecla premida
 teclado:
 	; constantes
-	LINHA      EQU 8		; linha a testar (4ª linha, 1000b)
+	LINHA      EQU 8		; linha a testar (4Âª linha, 1000b)
 	MASCARA    EQU 0FH		; para isolar os 4 bits de menor peso
 
-	; inicializações
-	MOV 	R1, LINHA		; linha inicial (4ª linha = 1000b)
-	MOV		R2, 0			; output do teclado (colunas)
-    MOV		R3, TEC_LIN   	; endereço do periférico das linhas
-    MOV  	R4, TEC_COL   	; endereço do periférico das colunas
-    MOV  	R5, DISPLAYS  	; endereço do periférico dos displays
-    MOV  	R6, MASCARA   	; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
-	MOV  	R7, 4         	; número de linhas
+	; inicializaÃ§Ãµes
+	MOV 	R1, LINHA		; linha inicial (4Âª linha = 1000b)
+	MOV	R2, 0			; output do teclado (colunas)
+   	MOV	R3, TEC_LIN   		; endereÃ§o do perifÃ©rico das linhas
+    	MOV  	R4, TEC_COL   		; endereÃ§o do perifÃ©rico das colunas
+    	MOV  	R5, DISPLAYS  		; endereÃ§o do perifÃ©rico dos displays
+    	MOV  	R6, MASCARA   		; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
+	MOV  	R7, 4         		; nÃºmero de linhas
 
-	; lê as 4 linhas do teclado
+	; lÃª as 4 linhas do teclado
 	le_linhas:
-		MOVB	[R3], R1	; escrever no periférico de saída (linhas)
-		MOVB	R2, [R4]    ; ler do periférico de entrada (colunas)
-		AND 	R2, R6      ; elimina bits para além dos bits 0-3
-		CMP  	R2, 0       ; há tecla premida?
-		JNZ		log_lin		; transfoma coluna/linha em m/n em vez de 2^m/n
-		CMP		R1, 0		; já chegou à linha 0?
-		JZ		return		; volta ao loop inicial
-		SHR		R1, 1		; se nenhuma tecla premida, repete (muda de linha)
-		JMP		le_linhas	; verifica próxima linha
+		MOVB	[R3], R1	; escrever no perifÃ©rico de saÃ­da (linhas)
+		MOVB	R2, [R4]    	; ler do perifÃ©rico de entrada (colunas)
+		AND 	R2, R6      	; elimina bits para alÃ©m dos bits 0-3
+		CMP  	R2, 0      	 ; hÃ¡ tecla premida?
+		JNZ	log_lin		; transfoma coluna/linha em m/n em vez de 2^m/n
+		CMP	R1, 0		; jÃ¡ chegou Ã  linha 0?
+		JZ	return		; volta ao loop inicial
+		SHR	R1, 1		; se nenhuma tecla premida, repete (muda de linha)
+		JMP	le_linhas	; verifica prÃ³xima linha
 		
 	; transfoma linha em n em vez de 2^n
 	log_lin:
 		SHR 	R1, 1		; funciona no caso de n = {0,1,2}
 		CMP 	R1, 4		; caso particular, n = 4, deveria ser 3
-		JNZ		log_col		;
-		SUB		R1, 1		; subtrai 1 a 4 para obter 3
+		JNZ	log_col		;
+		SUB	R1, 1		; subtrai 1 a 4 para obter 3
 	
 	; transfoma coluna em m em vez de 2^m
 	log_col:
 		SHR 	R2, 1		; funciona no caso de m = {0,1,2}
 		CMP 	R2, 4		; caso particular, m = 4, deveria ser 3
-		JNZ		cria_hex	;
-		SUB		R2, 1		; subtrai 1 a 4 para obter 3
+		JNZ	cria_hex	;
+		SUB	R2, 1		; subtrai 1 a 4 para obter 3
 		
-	; transforma input do periférico em valor hexadecimal
+	; transforma input do perifÃ©rico em valor hexadecimal
 	cria_hex:
-		MUL 	R1, R7		; fórmula para obter valor hexadecimal a partir do output
-		ADD 	R1, R2		; dos periféricos: coluna + linha * 4
-		MOV		R0, R1		; output da rotina em R0
+		MUL 	R1, R7		; fÃ³rmula para obter valor hexadecimal a partir do output
+		ADD 	R1, R2		; dos perifÃ©ricos: coluna + linha * 4
+		MOV	R0, R1		; output da rotina em R0
 	
 	; volta ao loop inicial
 	return:
